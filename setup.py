39bf8cc08d20b75f06234f2a0f3bd7f29aa9d5d4
Stephen Mercier <stephen.mercier@attainia.com>
2018-03-01 10:14:47 -0700

    Basic setup file for the project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tf_api_gateway',
+      version='0.1',
+      description='Wrapper for the Terraform Enterprise API',
+      url='http://github.com/PlethoraOfHate/tf_api_gateway',
+      author='Stephen Mercier',
+      author_email='stephen.mercier@gmail.com',
+      license='Apache License 2.0',
+      packages=['tf_api_gateway'],
+      zip_safe=False)
