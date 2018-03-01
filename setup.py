270bf3cc36265ccc2e01407d883bdd800119e846
Stephen Mercier <stephen.mercier@attainia.com>
2018-03-01 11:36:21 -0700

    Added readme method, bumped version, added download url, added keywords, added classifiers

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='tf_api_gateway',
-      version='0.1',
+      version='0.2',
       description='Wrapper for the Terraform Enterprise API',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache License 2.0',
+        'Programming Language :: Python :: 3.5',
+      ],
       url='http://github.com/PlethoraOfHate/tf_api_gateway',
+      download_url='https://github.com/PlethoraOfHate/tf_api_gateway/archive/0.1.tar.gz',
       author='Stephen Mercier',
       author_email='stephen.mercier@gmail.com',
       license='Apache License 2.0',
+      keywords=['terraform', 'atlas'],
       packages=['tf_api_gateway'],
+      include_package_data=True,
       zip_safe=False)
